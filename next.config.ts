171eedfec641d8500193ccb1d4a94a68e8d9acbc
Donato Carpentiere <d.carpentiere@sabanet.it>
2026-04-13 12:40:29 +0200

    fix: update Next.js configuration for static site generation and image optimization

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  /* le tue eventuali configurazioni */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
